5a1e5c9aae83906d2991c9a9c7cc5aa96ac3c0aa
Christopher I. Jamil <87200142+KillswitchPrime@users.noreply.github.com>
2022-10-20 14:20:08 +0200

    Renamed i to something more relevant and readable

--- Primes/Primes/PrimeSearcher.cs
+++ Primes/Primes/PrimeSearcher.cs
@@ -10,13 +10,13 @@ namespace Primes
 
             var primes = new List<int> { 3, 5, 7, 11, 13, 17};
 
-            for(var i = 19; i < primeSearchLimit; i += 2)
+            for(var primeCandidate = 19; primeCandidate < primeSearchLimit; primeCandidate += 2)
             {
                 var isPrime = true; 
 
-                for(var index = 0; index < primes.Count && isPrime && primes[index] < Math.Sqrt(i); index++)
+                for(var index = 0; index < primes.Count && isPrime && primes[index] < Math.Sqrt(primeCandidate); index++)
                 {
-                    if(i % primes[index] == 0)
+                    if(primeCandidate % primes[index] == 0)
                     {
                         isPrime = false;
                     }
@@ -24,7 +24,7 @@ namespace Primes
 
                 if (isPrime)
                 {
-                    primes.Add(i);
+                    primes.Add(primeCandidate);
                 }
             }
 
