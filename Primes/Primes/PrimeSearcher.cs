b6ac5f8a2a67592b174091859c9ce01d759cfa00
Christopher I. Jamil <87200142+KillswitchPrime@users.noreply.github.com>
2022-10-20 14:18:08 +0200

    Moved start point up 1 prime

--- Primes/Primes/PrimeSearcher.cs
+++ Primes/Primes/PrimeSearcher.cs
@@ -8,9 +8,9 @@ namespace Primes
         {
             if(primeSearchLimit < 20) primeSearchLimit = 20;
 
-            var primes = new List<int> { 3, 5, 7, 11, 13};
+            var primes = new List<int> { 3, 5, 7, 11, 13, 17};
 
-            for(var i = 17; i < primeSearchLimit; i += 2)
+            for(var i = 19; i < primeSearchLimit; i += 2)
             {
                 var isPrime = true; 
 
